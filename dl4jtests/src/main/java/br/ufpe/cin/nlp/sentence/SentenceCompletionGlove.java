0eb4f16e4a75e8085826e8b720cb9c9842d75372
srmq <srmq@cin.ufpe.br>
2015-09-28 15:36:13 +0000

    work in progress

--- dl4jtests/src/main/java/br/ufpe/cin/nlp/sentence/SentenceCompletionGlove.java
+++ dl4jtests/src/main/java/br/ufpe/cin/nlp/sentence/SentenceCompletionGlove.java
@@ -6,18 +6,18 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.compress.compressors.gzip.GzipUtils;
-import org.deeplearning4j.models.embeddings.inmemory.InMemoryLookupTable;
+import org.deeplearning4j.models.glove.Glove;
+import org.deeplearning4j.models.glove.GloveWeightLookupTable;
 import org.deeplearning4j.models.word2vec.VocabWord;
 import org.deeplearning4j.models.word2vec.Word2Vec;
 import org.deeplearning4j.models.word2vec.wordstore.VocabCache;
 import org.deeplearning4j.models.word2vec.wordstore.inmemory.InMemoryLookupCache;
+import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.rng.Random;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.ops.transforms.Transforms;
 import org.slf4j.Logger;
@@ -26,57 +26,67 @@ import org.slf4j.LoggerFactory;
 public class SentenceCompletionGlove {
 	
     private static final Logger log = LoggerFactory.getLogger(SentenceCompletionGlove.class);
-	
-	private static Word2Vec readTextModel(File modelFile, Set<String> sentenceWords)
-            throws IOException, NumberFormatException {
-
+    
+    private SentenceIterator iter;
+    
+    public SentenceCompletionGlove(SentenceIterator sentenceIterator) {
+    	this.iter = sentenceIterator;
+    }
+    
+	public static Glove readFullGloveTextModel(File modelFile, SentenceIterator iter,
+			boolean symmetric, boolean shuffle, int minWordFrequency, int iterations, double learningRate,
+			int layerSize) throws IOException, NumberFormatException {
+    	
 		final BufferedReader reader = getTextFileModelReader(modelFile);
 		
         String line = reader.readLine();
         String[] initial = line.split(" ");
         final int words = Integer.parseInt(initial[0]);
-        final int layerSize = Integer.parseInt(initial[1]);
+        final int fileLayerSize = Integer.parseInt(initial[1]);
         
-        List<String> wordsWithVectors = new ArrayList<String>(sentenceWords.size());
-        List<float[]> embeddings = new ArrayList<float[]>(sentenceWords.size());
+        assert (layerSize == fileLayerSize);
+        
+        
+    	VocabCache cache = new InMemoryLookupCache(true);
+        INDArray syn0 = Nd4j.create(words + 1, layerSize);
+        Random rng = Nd4j.getRandom();
+        { // init embeddings for UNKNOWN word to random values
+	        INDArray randUnk = Nd4j.rand(1, layerSize, rng).subi(0.5).divi(layerSize);
+	        final int idx = cache.indexOf(Word2Vec.UNK);
+	        syn0.slice(idx).assign(randUnk);        
+        }
+    	
+        int index = cache.numWords();
         while ((line = reader.readLine()) != null) {
             String[] split = line.split(" ");
             assert split.length == layerSize + 1;
             String word = split[0];
-            if (sentenceWords.contains(word)) {
-            	wordsWithVectors.add(word);
-                float[] vector = new float[split.length - 1];
-                for (int i = 1; i < split.length; i++) {
-                    vector[i - 1] = Float.parseFloat(split[i]);
-                }
-                embeddings.add(vector);
+            float[] vector = new float[split.length - 1];
+            for (int i = 1; i < split.length; i++) {
+                vector[i - 1] = Float.parseFloat(split[i]);
             }
-        }
-		reader.close();
-		final int newWords = wordsWithVectors.size();
-		log.info("Total words in vocabulary: {}. Will use {}.", words, newWords);
-		
-		INDArray syn0 = Nd4j.create(newWords, layerSize);
-		VocabCache cache = new InMemoryLookupCache(false);
-		for (int i = 0; i < newWords; i++) {
-			final String word = wordsWithVectors.get(i);
-			final float[] embed = embeddings.get(i);
-			syn0.putRow(i, Transforms.unitVec(Nd4j.create(embed)));
-            cache.addWordToIndex(cache.numWords(), word);
+			syn0.putRow(index, Transforms.unitVec(Nd4j.create(vector)));
+            cache.addWordToIndex(index, word);
             cache.addToken(new VocabWord(1, word));
             cache.putVocabWord(word);
-		}
-		InMemoryLookupTable lookupTable = (InMemoryLookupTable) new InMemoryLookupTable.Builder().
-                cache(cache).vectorLength(layerSize).build();
-		lookupTable.setSyn0(syn0);
-		
-		Word2Vec ret = new Word2Vec();
-        ret.setVocab(cache);
-        ret.setLookupTable(lookupTable);
+            index++;
+        }
+		reader.close();
 
+		log.info("Total words in vocabulary: {}", cache.numWords());
 		
-		return ret;
-	}
+		GloveWeightLookupTable gloveTable = (new GloveWeightLookupTable.Builder().gen(rng).cache(cache)
+				.vectorLength(layerSize).lr(learningRate)).build();
+		gloveTable.setSyn0(syn0);
+		gloveTable.resetWeights(false);
+		gloveTable.setSyn1(Nd4j.create(syn0.shape()));
+		
+		Glove ret = (new Glove.Builder().cache(cache).iterations(iterations).iterate(iter).layerSize(layerSize)
+				.learningRate(learningRate).minWordFrequency(minWordFrequency).shuffle(shuffle).symmetric(symmetric))
+						.build();
+    	return ret; 
+    }    
+	
 
 	private static BufferedReader getTextFileModelReader(File modelFile) throws IOException, FileNotFoundException {
 		final BufferedReader reader = new BufferedReader(new InputStreamReader(
@@ -87,9 +97,10 @@ public class SentenceCompletionGlove {
 	}
 
 	public static void main(String[] args) throws IOException {
-		SentenceCompletionWords words = new SentenceCompletionWords(); 
-		Word2Vec vecModel = SentenceCompletionGlove.readTextModel(new File("/home/srmq/devel/glove/glove.6B.300d-WITHHEADERLINE.txt.gz"), words.words());
-		log.info("Vector model loaded with vocabulary size: {}", vecModel.vocab().numWords());
+		//FIXME todo
+		//SentenceCompletionWords words = new SentenceCompletionWords(); 
+		//Glove gloveModel = SentenceCompletionGlove.readFullGloveTextModel(new File("/home/srmq/devel/glove/glove.6B.300d-WITHHEADERLINE.txt.gz"), );
+		//log.info("Vector model loaded with vocabulary size: {}", vecModel.vocab().numWords());
 	}
 
 }
