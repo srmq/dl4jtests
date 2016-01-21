d27e4c9498ec63fc98d0370dcab52779cf56871d
srmq <srmq@cin.ufpe.br>
2016-01-21 17:42:20 -0300

    refactor to use in the generation of random questions

--- dl4jtests/src/main/java/br/ufpe/cin/nlp/sentence/HolmesDistanceDatasetGenerator.java
+++ dl4jtests/src/main/java/br/ufpe/cin/nlp/sentence/HolmesDistanceDatasetGenerator.java
@@ -1,6 +1,7 @@
 package br.ufpe.cin.nlp.sentence;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -11,6 +12,33 @@ import br.ufpe.cin.nlp.sentence.base.SentenceCompletionQuestions;
 import br.ufpe.cin.nlp.sentence.base.SentenceCompletionQuestions.Question;
 
 public class HolmesDistanceDatasetGenerator {
+	
+	private Iterator<Question> questionIt;
+	private SentenceCompletionAnswering scAns;
+	
+	public HolmesDistanceDatasetGenerator(SentenceCompletionAnswering scAns, Iterator<Question> questionIt) throws IOException {
+		assert(questionIt != null);
+		this.questionIt = questionIt;
+		this.scAns = scAns;
+	}
+	
+	public void printDistances(PrintStream stream) {
+		int qNum = 1;
+		while(questionIt.hasNext()) {
+			Question q = questionIt.next();
+			stream.print(qNum);
+			Map<DistanceType, INDArray[]> dTypeToDistances = scAns.computeAllDistancesForQuestion(q);
+			for (INDArray[] distArray : dTypeToDistances.values()) {
+				for (int i = 0; i < distArray.length; i++) {
+					for (int j = 0; j < distArray[i].length(); j++) {
+						stream.print(" " + distArray[i].getDouble(j));
+					}
+				}
+			}
+			stream.println(" " + q.getCorrectIndex());
+			qNum++;
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		SentenceCompletionAnswering scAns = new SentenceCompletionAnswering(
@@ -27,22 +55,9 @@ public class HolmesDistanceDatasetGenerator {
 						"TfIdfInfo-Holmes-Glove-StopwordsRemoved.json.gz" ,
 						"TfIdfInfo-Holmes-SennaOriginalVectors-StopwordsPresent.json.gz" });
 		final SentenceCompletionQuestions questions = new SentenceCompletionQuestions();
-		int qNum = 1;
-		for (Iterator<Question> it = questions.getQuestions().iterator(); it.hasNext();) {
-			Question q = it.next();
-			System.out.print(qNum);
-			Map<DistanceType, INDArray[]> dTypeToDistances = scAns.computeAllDistancesForQuestion(q);
-			for (INDArray[] distArray : dTypeToDistances.values()) {
-				for (int i = 0; i < distArray.length; i++) {
-					for (int j = 0; j < distArray[i].length(); j++) {
-						System.out.print(" " + distArray[i].getDouble(j));
-					}
-				}
-			}
-			System.out.println(" " + q.getCorrectIndex());
-			qNum++;
-		}
-
+		Iterator<Question> it = questions.getQuestions().iterator();
+		HolmesDistanceDatasetGenerator generator = new HolmesDistanceDatasetGenerator(scAns, it);
+		generator.printDistances(System.out);
 	}
 
 }
