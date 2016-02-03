eff5a7d94bb515b940380dfbbaebf62198a86aeb
srmq <srmq@cin.ufpe.br>
2016-02-03 11:06:35 -0300

    rename phase 2 deleting old directory

--- dl4jtests/src/main/java/br/ufpe/cin/nlp/sentence/VectorVocab.java
+++
@@ -1,8 +0,0 @@
-package br.ufpe.cin.nlp.sentence;
-
-public interface VectorVocab {
-	public float[] embeddingFor(String word);
-	public boolean contains(String word);
-	public int numWords();
-	public int embedSize();
-}
